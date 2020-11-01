26009184fa6a56c5a92a78d7a10a121bd1e7f94e
Justin Movitz <movitzj1@montclair.edu>
2020-11-01 21:34:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |inner_key, inner_value|
       inner_value.each do |name|
-        if empty_hash.keys.index(name) == nil
-          empty_hash.store(name, {key => [inner_key.to_s]})
+        if empty_hash.keys.index(name) == nil #Reference https://ruby-doc.org/core-2.5.1/Hash.html
+          empty_hash.store(name, {key => [inner_key.to_s]}) # Reference https://ruby-doc.org/core-2.5.1/Hash.html
         else
           if empty_hash[name].keys.index(key) == nil
             empty_hash[name].store(key, [inner_key.to_s])
