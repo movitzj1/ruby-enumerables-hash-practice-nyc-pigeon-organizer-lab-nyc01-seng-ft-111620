64d64b51baad667b21c914307f4016cce5972d4a
Justin Movitz <movitzj1@montclair.edu>
2020-11-04 20:31:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,11 +6,11 @@ def nyc_pigeon_organizer(data)
         if empty_hash.keys.index(name) == nil #Reference https://ruby-doc.org/core-2.5.1/Hash.html
           empty_hash.store(name, {key => [inner_key.to_s]}) # Reference https://ruby-doc.org/core-2.5.1/Hash.html
         else
-          #if empty_hash[name].keys.index(key) == nil
-            #empty_hash[name].store(key, [inner_key.to_s])
-          #else
-          empty_hash[name][key] << inner_key.to_s
-          #end
+          if empty_hash[name].keys.index(key) == nil
+            empty_hash[name].store(key, [inner_key.to_s])
+          else
+            empty_hash[name][key] << inner_key.to_s
+          end
         end
       end
     end
